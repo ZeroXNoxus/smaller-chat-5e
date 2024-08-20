746d45da2bb095035665d2c823f9779b8cedd798
Nikolai Kucksdorf <nikolai.kucksdorf@live.de>
2024-08-20 17:14:44 +0200

    Module Management + Support

--- script/smaller-chat.js
+++ script/smaller-chat.js
@@ -1,7 +1,18 @@
+const moduleList = [
+    'module-credits'
+];
+const body = 'body.vtt';
+
+function moduleSupport(name){
+    game.modules.get(name) ? $(body).addClass(name+'-enabled') : '';
+}
 function changeMode(val, name){
-    if(val == true){
-        $('body.vtt').addClass(name);
-    }
+    if(val == true){ 
+        $(body).addClass(name);
+        for(let i = 0; i < moduleList.length; i++){
+            moduleSupport(moduleList[i]);
+        }
+    };
 }
 Hooks.once('init', () => {
     changeMode(true, 'zetas-smaller-chat-5e');
